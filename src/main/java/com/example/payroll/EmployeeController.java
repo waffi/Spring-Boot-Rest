dcb6ef9444720b140be33c8d31106b6a2cf04f07
Waffi Fatur Rahman <faturrahmanwaffi@gmail.com>
2022-04-06 14:21:13 +0700

    Simplify links creation

--- src/main/java/com/example/payroll/EmployeeController.java
+++ src/main/java/com/example/payroll/EmployeeController.java
@@ -19,9 +19,13 @@ import org.springframework.web.bind.annotation.RestController;
 class EmployeeController {
 
   private final EmployeeRepository repository;
+  
+  private final EmployeeModelAssembler assembler;
 
-  EmployeeController(EmployeeRepository repository) {
+  EmployeeController(EmployeeRepository repository, EmployeeModelAssembler assembler) {
+
     this.repository = repository;
+    this.assembler = assembler;
   }
 
 
@@ -30,10 +34,8 @@ class EmployeeController {
   @GetMapping("/employees")
   CollectionModel<EntityModel<Employee>> all() {
 
-    List<EntityModel<Employee>> employees = repository.findAll().stream()
-        .map(employee -> EntityModel.of(employee,
-            linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
-            linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+    List<EntityModel<Employee>> employees = repository.findAll().stream()		
+        .map(assembler::toModel) // .map(employee -> assembler.toModel(employee))
         .collect(Collectors.toList());
 
     return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
@@ -53,9 +55,7 @@ class EmployeeController {
     Employee employee = repository.findById(id) //
         .orElseThrow(() -> new EmployeeNotFoundException(id));
 
-    return EntityModel.of(employee, //
-        linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
-        linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+    return assembler.toModel(employee);
   }
 
   @PutMapping("/employees/{id}")
